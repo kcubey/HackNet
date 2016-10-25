658eae5a76c169383fb339077671241debd97f49
Wen Liang - Laptop <wenlianggg@gmail.com>
2016-10-25 17:26:14 +0800

    lotsa changes

--- HackNet/Game.Master.designer.cs
+++ HackNet/Game.Master.designer.cs
@@ -10,7 +10,7 @@
 namespace HackNet {
     
     
-    public partial class _Default {
+    public partial class GameMaster {
         
         /// <summary>
         /// PlayerName control.
@@ -47,5 +47,14 @@ namespace HackNet {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label BucksLbl;
+        
+        /// <summary>
+        /// GameContent control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder GameContent;
     }
 }
---
+++ HackNet/Game/Home.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Game.Master" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="HackNet.Game.Home" %>
+<asp:Content ID="HomePageContent" ContentPlaceHolderID="GameContent" runat="server">
+
+    <div class="panel panel-default">
+        <div class="panel panel-body">
+            <p>Welcome back to HackNet!
+            <br />
+            <br />
+                HERE ARE SOME ANNOUNCEMENTS..<span class="blinking">_</span>
+            </p>
+        </div>
+    </div>
+</asp:Content>
