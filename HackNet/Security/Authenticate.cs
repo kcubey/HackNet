910baaaf66cc4ed1125f08d7d68185565d397791
Wen Liang - Laptop <wenlianggg@gmail.com>
2016-11-01 12:58:23 +0800

    Auth changes

--- HackNet/Security/Authenticate.cs
+++ HackNet/Security/Authenticate.cs
@@ -1,22 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Web;
 
-namespace HackNet.Security {
-    public class Authenticate {
-
-        internal static string Hash(string password, string salt = null) {
-
-            using (SHA256 s256 = SHA256.Create())
-            using (SHA512 s512 = SHA512.Create())  {
-                
-
+namespace HackNet.Security
+{
+    public class Authenticate
+    {
+        internal static string Hash(string password, string salt = null)
+        {
+            using (SHA512 s512 = SHA512.Create())
+            {
+                MemoryStream ms = new MemoryStream();
+                CryptoStream cs = new CryptoStream(ms, s512, CryptoStreamMode.Write);
 
             }
-
             return password;
+        }
+
+        internal static byte[] Generate(int length)
+        {
+            if (length == 0) // Guard clause
+                return null;
+            byte[] random = new byte[length];
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+                rng.GetBytes(random);
+            return random;
         }
 
     }
