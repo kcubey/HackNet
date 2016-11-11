691894be903e21d6b7640dcdc7c1e96dba556364
Wen Liang - Laptop <wenlianggg@gmail.com>
2016-11-11 16:11:59 +0800

    Workstation = new Hackspace

--- HackNet/Game.Master.cs
+++ HackNet/Game.Master.cs
@@ -5,10 +5,51 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace HackNet {
-    public partial class GameMaster : MasterPage {
-        protected void Page_Load(object sender, EventArgs e) {
+namespace HackNet
+{
+    public partial class GameMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
 
         }
+
+        #region Navigation Links
+        protected void LnkAnn(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Game/Home");
+        }
+
+        protected void LnkWsn(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Game/Home");
+        }
+
+        protected void LnkMis(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Game/Missions");
+        }
+
+        protected void LnkEvt(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Game/Home");
+        }
+
+        protected void LnkMLog(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Game/Home");
+        }
+
+        protected void LnkInv(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Game/Home");
+        }
+
+        protected void LnkMkt(object sender, EventArgs e)
+        {
+            Response.Redirect("~/Game/Market");
+        }
+        #endregion
+
     }
 }
