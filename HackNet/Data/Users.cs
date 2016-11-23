3b2a92bcb16e5527546c998353870ad86a6f5451
Wen Liang - Desktop <wenlianggg@gmail.com>
2016-11-24 03:00:12 +0800

    Minor changes

--- HackNet/Data/Users.cs
+++ HackNet/Data/Users.cs
@@ -13,17 +13,27 @@ namespace HackNet.Data
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int UserID { get; set; }
 
+		[Required]
+		[Display(Name = "User Name")]
+		[StringLength(15, ErrorMessage = "Username must be between 4 to 15 characters", MinimumLength = 4)]
         public string UserName { get; set; }
 
+		[Required]
+		[Display(Name = "Full Name")]
+		[StringLength(50, ErrorMessage = "Full name must be between 5 to 50 characters", MinimumLength = 5)]
         public string FullName { get; set; }
 
-        public string Email { get; set; }
+		[Required]
+		[Display(Name = "Email address")]
+		[EmailAddress(ErrorMessage = "Ensure that the email address is properly entered!")]
+		public string Email { get; set; }
    
         public string Hash { get; set; }
 
         public string Salt { get; set; }
 
-        public DateTime BirthDate { get; set; }
+		[DataType(DataType.Date)]
+		public DateTime BirthDate { get; set; }
 
         public DateTime Registered { get; set; }
 
