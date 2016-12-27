8a83ff12c852bde1e1c6055b1f100cd7a1cada9f
Wen Liang - Desktop <wenlianggg@gmail.com>
2016-12-28 02:05:03 +0800

    Skeleton for logger

---
+++ HackNet/Loggers/Logger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HackNet.Loggers
+{
+	public class Logger
+	{
+		private static Logger _instance;
+
+		private Logger()
+		{
+
+		}
+
+		internal Logger Instance
+		{
+			get
+			{
+				if (_instance == null)
+					_instance = new Logger();
+				return _instance;
+			}
+		}
+
+	}
+}
