23adc114fa547f9c209fc29705c1ab74f57f51a6
Wen Liang - Desktop <wenlianggg@gmail.com>
2017-01-17 03:17:33 +0800

    wtf chat

--- HackNet/Game/Chat.aspx.designer.cs
+++ HackNet/Game/Chat.aspx.designer.cs
@@ -31,6 +31,24 @@ namespace HackNet.Game {
         protected global::System.Web.UI.WebControls.Button ButtonChooseRecipient;
         
         /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Button1;
+        
+        /// <summary>
+        /// ButtonLoadChat control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ButtonLoadChat;
+        
+        /// <summary>
         /// Msg control.
         /// </summary>
         /// <remarks>
