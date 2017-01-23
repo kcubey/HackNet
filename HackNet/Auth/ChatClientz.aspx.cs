cf5f71e2eadb0655c74fcbe738b2291965540211
Wen Liang - Desktop <wenlianggg@gmail.com>
2017-01-24 02:58:42 +0800

    Lotsa changes

---
+++ HackNet/Auth/ChatClientz.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HackNet.SignalR
+{
+	public partial class ChatClientz : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
---
+++ HackNet/SignalR/MessageHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace Microsoft.AspNet.SignalR.Messaging
+{
+    [HubName("ChatClientz")]
+    public class MessageHub : Hub
+    {
+        private readonly MessageSignal _msg;
+
+        public MessageHub()
+        {
+
+        }
+
+        public void SendMessage(string name, string msg)
+        {
+			Clients.All.broadcastMessage(name, msg);
+        }
+    }
+}
