198ea067b7c4925e56f760e7a343df556a8128c7
Wen Liang - Laptop <wenlianggg@gmail.com>
2017-01-26 11:46:26 +0800

    Fix open redirect vuln

--- HackNet/Auth/SignIn.aspx.cs
+++ HackNet/Auth/SignIn.aspx.cs
@@ -54,15 +54,15 @@ namespace HackNet.Auth {
 					Session["TempKeyStore"] = ks;
 					Session["Cookie"] = a.AuthCookie;
 					Session["PasswordSuccess"] = email;
-					Session["ReturnUrl"] = Request.QueryString["ReturnUrl"];
+					Session["ReturnUrl"] = "~" + Request.QueryString["ReturnUrl"];
 					Response.Redirect("~/Auth/OtpVerify");
 				} else
 				{
 					Session["KeyStore"] = ks;
-					Session["ReturnUrl"] = null;
+					string redir = Request.QueryString["ReturnUrl"];
 					Response.Cookies.Add(a.AuthCookie);
 					if (Request.QueryString["ReturnUrl"] != null)
-						Response.Redirect("~" + Request.QueryString["ReturnUrl"]);
+						Response.Redirect("~" + redir);
 					else
 						Response.Redirect("~/Default");
 				}
